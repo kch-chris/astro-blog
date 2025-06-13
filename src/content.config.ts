8d28147ea0af9e26025dbebaf3c1328d2f58ccad
Kch Murillo <kchton4@gmail.com>
2025-06-13 15:09:27 -0600

    Finished Section 7

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
     type: 'content',
@@ -8,12 +8,28 @@ const blogCollection = defineCollection({
         date: z.date(),
         image: image(),
         // Relationship to authors
-        author: z.string(),
+        author: reference('author'),
         // Relationship
         tags: z.array(z.string()),
+
+        //Boolean
+        isDraft: z.boolean().default(false),
     })
 });
 
+const authorCollection = defineCollection({
+    type: 'data',
+    schema: ({ image}) =>  z.object({
+        name: z.string(),
+        avatar: image(),
+        twitter: z.string(),
+        linkedIn: z.string(),
+        github: z.string(),
+        bio: z.string(),
+        subtitle: z.string(),
+    })
+});
 export const collections = {
     blog: blogCollection,
+    author: authorCollection
 };
--- src/content/blog/post-04.md
+++ src/content/blog/post-04.md
@@ -2,9 +2,10 @@
 title: Construyendo una API REST con Node.js
 date: 2023-06-20
 description: En este tutorial, construiremos una API RESTful utilizando Node.js y Express.
-author: Bob Williams
+author: bob-williams
 image: 'images/post-04.png'
 tags: [Node.js, Express, API, Backend]
+isDraft: false
 ---
 
 # Construyendo una API REST con Node.js
